d46a6e61d6876c2321e0d9f72f7b42633ef3ccac
Artem <artemklishch@gmail.com>
2020-02-05 15:53:27 +0200

    t2l4

--- lesson4/task2/src/Clock.jsx
+++ lesson4/task2/src/Clock.jsx
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
-
+import moment from 'moment';
 import './clock.scss';
+
+
+const localeTime = (offset) => {
+  const currentTime = new Date();
+  const currentTimeWthOffset = currentTime.setHours(currentTime.getHours() + offset);
+  return moment(currentTimeWthOffset).format('LTS');
+};
 
 class Clock extends Component{
   constructor(props){
     super(props);
     this.state = {
-
+      time: localeTime(props.offset)
     };
+    setInterval(() => {
+      this.setState({
+        time: localeTime(props.offset)
+      });
+    }, 1000);
   }
   render(){
-    <div className="clock">
-  <p className="clock__location">{this.props.location}</p>
-      <p className="clock__time"></p>
-    </div>
+    return (
+      <div className="clock">
+        <p className="clock__location">{this.props.location}</p>
+        <p className="clock__time">{this.state.time}</p>
+      </div>
+    );
   }
 }
 export default Clock;
--- lesson4/task2/src/index.jsx
+++ lesson4/task2/src/index.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import moment from 'moment';
 import './index.scss';
 
 const rootElement = document.querySelector('#root');
 
-/// ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App />, rootElement);
