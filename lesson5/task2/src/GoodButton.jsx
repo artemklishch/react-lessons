51186fe5a61c2928c2921a76a2fc47407172d7be
Artem <artemklishch@gmail.com>
2020-02-05 19:14:50 +0200

    erwer

---
+++ lesson5/task2/src/GoodButton.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class GoodButton extends React.Component{
+  handleClick(e){
+    console.log(e.screenX);
+    console.log(e);
+    alert('Good job!');
+  }
+  render(){
+    return (
+      <button className="fancy-button" onClick={this.handleClick}>
+        Click me!
+      </button>
+    );
+  }
+}
+export default GoodButton;
