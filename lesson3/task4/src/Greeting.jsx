a893b674210e9911c127156a34050c4adabb3e77
Artem <artemklishch@gmail.com>
2020-02-05 09:41:21 +0200

    t4l3

--- lesson3/task4/src/Greeting.jsx
+++ lesson3/task4/src/Greeting.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import moment from 'moment';
 
-const onGetFullYears = date => moment(date, "YYYYMMDD").fromNow().substr(0,2);
+const fullYears = date => moment(date, "YYYYMMDD").fromNow().substr(0,2);
 
 const Greeting = props => {
   console.log(props);
   return (
   <div className="greeting">
-    {`My name is ${props.firstName} ${props.lastName}. I'm ${onGetFullYears(props.birthDate)} years old.`}
+    {`My name is ${props.firstName} ${props.lastName}. I'm ${fullYears(props.birthDate)} years old.`}
   </div>
   );
 };
