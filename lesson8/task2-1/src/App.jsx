91fa43fdac1dc9261acb02ab6e9c17fab42af778
Artem <artemklishch@gmail.com>
2020-02-08 21:13:08 +0200

    t2l8

---
+++ lesson8/task2-1/src/App.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Clock from './Clock';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: true,
+    };
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      visible: !this.state.visible
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <button onClick={this.toggle}>Toggle</button>
+        {
+          this.state.visible &&
+          <div className="forClocks">
+            <Clock location="London" offset={-2} />
+            <Clock location="Kyiv" offset={0} />
+            <Clock location="New York" offset={-7} />
+          </div>
+        }
+      </>
+    );
+  }
+};
+export default App;
---
+++ lesson8/task2-1/src/Clock.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+
+
+class Clock extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      time: this.localeTime(props.offset)
+    };
+    
+  }
+
+  componentDidMount(){
+    this.interval = setInterval(() => {
+      this.setState({
+        time: this.localeTime(this.props.offset)
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.interval);
+  }
+
+  localeTime = (offset) => {
+    const currentTime = new Date();
+    const currentTimeWthOffset = currentTime.setHours(currentTime.getHours() + offset);
+    return moment(currentTimeWthOffset).format('LTS');
+  };
+  render(){
+    return (
+      <div className="clock">
+        <p className="clock__location">{this.props.location}</p>
+        <p className="clock__time">{this.state.time}</p>
+      </div>
+    );
+  }
+}
+export default Clock;
