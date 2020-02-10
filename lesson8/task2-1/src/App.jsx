05ddf856ae07b1557e5ade9dadaca872f8d60412
Artem <artemklishch@gmail.com>
2020-02-10 21:16:13 +0200

    try

--- lesson8/task2-1/src/App.jsx
+++ lesson8/task2-1/src/App.jsx
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import Clock from './Clock';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      visible: true,
-    };
-    //this.toggle = this.toggle.bind(this);
-  }
-
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     visible: true,
+  //   };
+  //   //this.toggle = this.toggle.bind(this);
+  // }
+  state = {
+    visible: true,
+  };
   // toggle() {
   //   this.setState({
   //     visible: !this.state.visible
@@ -18,17 +20,24 @@ class App extends Component {
 
   render() {
     return (
-      <>
-        {/* <button onClick={this.toggle}>Toggle</button> */}
-        {
-          this.state.visible &&
-          <div className="forClocks">
-            <Clock location="London" offset={-2} />
-            <Clock location="Kyiv" offset={0} />
-            <Clock location="New York" offset={-7} />
-          </div>
-        }
-      </>
+      this.state.visible &&
+      <div className="forClocks">
+        <Clock location="London" offset={-2} />
+        <Clock location="Kyiv" offset={0} />
+        <Clock location="New York" offset={-7} />
+      </div>
+
+      // <>
+      //   {/* <button onClick={this.toggle}>Toggle</button> */}
+      //   {
+      //     this.state.visible &&
+      //     <div className="forClocks">
+      //       <Clock location="London" offset={-2} />
+      //       <Clock location="Kyiv" offset={0} />
+      //       <Clock location="New York" offset={-7} />
+      //     </div>
+      //   }
+      // </>
     );
   }
 };
