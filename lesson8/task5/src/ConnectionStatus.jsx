b65882ae96ee529908257e50e03be621f19b83ab
Artem <artemklishch@gmail.com>
2020-02-10 21:44:36 +0200

    wer

--- lesson8/task5/src/ConnectionStatus.jsx
+++ lesson8/task5/src/ConnectionStatus.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 
 class ConnectionStatus extends Component {
   state = {
-    isOnline: true
+    //isOnline: true
+    isOnline: 'Online',
   };
   componentDidMount(){
     window.addEventListener('online', this.handleOnlineHeader);
@@ -12,23 +13,37 @@ class ConnectionStatus extends Component {
     window.removeEventListener('online', this.handleOnlineHeader);
     window.removeEventListener('offline', this.handleOfflineHeader);
   }
+  // handleOnlineHeader = () => {
+  //   this.setState({
+  //     isOnline: true
+  //   });
+  // };
+  // handleOfflineHeader = () => {
+  //   this.setState({
+  //     isOnline: false
+  //   });
+  // };
   handleOnlineHeader = () => {
+    const elem = document.querySelector('.status');
+    elem.classList.remove('status_offline');
     this.setState({
-      isOnline: true
+      isOnline: 'Online'
     });
   };
   handleOfflineHeader = () => {
+    const elem = document.querySelector('.status');
+    elem.classList.add('status_offline');
     this.setState({
-      isOnline: false
+      isOnline: 'Offline'
     });
   };
   render() {
-    return (
-      this.state.isOnline === true 
-        ? <div className="status">Online</div>
-        : <div className="status status_offline">Offline</div>
-    );
+    // return (
+    //   this.state.isOnline === true 
+    //     ? <div className="status">Online</div>
+    //     : <div className="status status_offline">Offline</div>
+    // );
+    return <div className="status">{this.state.isOnline}</div>
   }
-
 };
 export default ConnectionStatus;
