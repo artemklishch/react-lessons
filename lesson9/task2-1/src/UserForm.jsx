abddf4b9ce2faa9fb04bba1081fba594bc26750e
Artem <artemklishch@gmail.com>
2020-02-10 10:57:43 +0200

    t2l9

---
+++ lesson9/task2-1/src/UserForm.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+class UserForm extends Component{
+  state = {
+    name: '',
+    student: '',
+    occupation: 'London',
+    about: ''
+  };
+
+  handledChange = event => {
+    const { name, value, type, checked } = event.target;
+    const val = type === 'checkbox'
+      ? checked
+      : value;
+    this.setState({
+      [name]: val
+    });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+    //console.log(this.state);
+  };
+
+  render(){
+    return (
+      <form className="login-form" onSubmit={this.onSubmit}>
+        <h1 className="form-title">Profile</h1>
+        <div className="form-control">
+          <label className="form-label" htmlFor="name">Name</label>
+          <input className="form-input" type="text" id="name" name="name" onChange={this.handledChange} value={this.state.value} />
+        </div>
+        <div className="form-control">
+          <label className="form-label" htmlFor="student">Student</label>
+          <input className="form-input" type="checkbox" id="student" name="student" onChange={this.handledChange} value={this.state.value} />
+        </div>
+        <div className="form-control">
+          <label className="form-label" id="occupation" htmlFor="occupation">Occupation</label>
+          <select name="occupation" className="form-input" onChange={this.handledChange} value={this.state.value}>
+            <option value="london">London</option>
+            <option value="new-york">New York</option>
+            <option value="coconut">Sidney</option>
+            <option value="mango">Berlin</option>
+          </select>
+        </div>
+        <div className="form-control">
+          <label className="form-label" id="about" htmlFor="about">About</label>
+          <textarea name="about" className="form-input" onChange={this.handledChange} value={this.state.value} />
+        </div>
+        <button className="submit-button" type="submit">Submit</button>
+      </form>
+    );
+  }
+}
+export default UserForm;
