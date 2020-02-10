b21c0a13bc1806746a36c3af919ceff35a2c58ee
Artem <artemklishch@gmail.com>
2020-02-10 09:24:17 +0200

    34

--- lesson9/task1/src/Search.jsx
+++ lesson9/task1/src/Search.jsx
@@ -5,11 +5,17 @@ class Search extends Component {
   handleChange = event => {
     this.setState({ value: event.target.value });
   }
+
+  search = event => {
+    event.preventDefault();
+    console.log(this.state.value);
+  };
+
   render() {
     return (
-      <form className="search">
+      <form className="search" onSubmit={this.search} >
         <input type="text" onChange={this.handleChange} value={this.state.value} className="search__input" />
-        <button className="search__button">Search</button>
+        <button className="search__button" type="submit">Search</button>
       </form>
     );
   }
