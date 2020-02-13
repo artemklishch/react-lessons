5596caaff562df5f480b30e8d1e01568ccd9ee9b
Artem <artemklishch@gmail.com>
2020-02-13 15:58:06 +0200

    dfg

--- lesson9/task2-1/src/index.jsx
+++ lesson9/task2-1/src/index.jsx
@@ -3,6 +3,10 @@ import ReactDOM  from 'react-dom';
 import './index.scss';
 import UserForm from './UserForm';
 
+const funcForSentData = data => {
+  console.log(data);
+};
+
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<UserForm />, rootElement);
+ReactDOM.render(<UserForm onSubmit={funcForSentData} />, rootElement);
