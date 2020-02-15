971f75fd26e1e502c58b6e83ce6b9738065da494
Artem <artemklishch@gmail.com>
2020-02-15 14:43:04 +0200

    sdf

---
+++ lesson10/task4/src/SearchBar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const SearchBar = ({
+  filterText,
+  inStockOnly,
+  onFilterTextChange,
+  onInStockChange
+}) => {
+    return (
+      <form>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={filterText}
+          onChange={onFilterTextChange}
+        />
+        <p>
+          <input
+            type="checkbox"
+            checked={inStockOnly}
+            onChange={onInStockChange}
+          />
+          {' '}
+          Only show products in stock
+        </p>
+      </form>
+    );
+}
+export default SearchBar;
