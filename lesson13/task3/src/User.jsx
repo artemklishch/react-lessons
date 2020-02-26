42df8d07171a99200aa714e976fdb24ef1d65771
Artem <artemklishch@gmail.com>
2020-02-26 19:20:44 +0200

    fdg

--- lesson13/task3/src/User.jsx
+++ lesson13/task3/src/User.jsx
@@ -26,16 +26,5 @@ class User extends Component {
       </div>
     );
   }
-
-
-  // return (
-  //   <div className="user">
-  //     <img alt="User Avatar" src="https://avatars1.githubusercontent.com/u/9919?v=4" className="user__avatar" />
-  //     <div className="user__info">
-  //       <span className="user__name">GitHub</span>
-  //       <span className="user__location">San Francisco,CA</span>
-  //     </div>
-  //   </div>
-  // );
 };
 export default User;
