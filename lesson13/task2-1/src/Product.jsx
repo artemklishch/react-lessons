9bc4434791585cc6120c89f6730c6df503338252
Artem <artemklishch@gmail.com>
2020-02-26 11:15:45 +0200

    rty

--- lesson13/task2-1/src/Product.jsx
+++ lesson13/task2-1/src/Product.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const Product = ({match}) => {
-  const product = match.params.productId;
+  console.log(match);
+  const product = match.path;
   return (
   <div className="product">{product}</div>
   );
