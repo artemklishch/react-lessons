e452e3099ffcd45cb2e850613abdc7f221de1e24
Artem <artemklishch@gmail.com>
2020-02-28 14:42:09 +0200

    t3l14

---
+++ lesson14/task3/src/Dimensions.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+
+const Dimensions = () => {
+  const { innerWidth, innerHeight } = window;
+  const [windowSizes, setWindowSizez] = useState({
+    innerWidth,
+    innerHeight
+  }); 
+  useEffect(() => {
+    const handleSizes = () => {
+      const { innerWidth, innerHeight } = window;
+      setWindowSizez({ innerWidth, innerHeight });
+    };
+    window.addEventListener('resize', handleSizes);
+
+    return () => {
+      window.removeEventListener('resize', handleSizes);
+    };
+  }, [windowSizes]);
+  return <div className="dimensions">{`${innerWidth} - ${innerHeight}`}</div>;
+};
+export default Dimensions;
