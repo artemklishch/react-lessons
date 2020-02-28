6195ef94d49cc44f97253abb86866ce5dda9e476
Artem <artemklishch@gmail.com>
2020-02-28 17:35:27 +0200

    sdf

--- lesson14/task5/src/App.jsx
+++ lesson14/task5/src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Clock from './Clock';
 
 const App = () => {
+  const [isVisible, setVisibility] = useState(true);
   return (
-    <div className="clock">
-      <Clock location="New York" offset={-5} />
-    </div>
+    <>
+      <button onClick={() => setVisibility(!isVisible)}>Toggle</button>
+      <div className="clock">
+        {
+          isVisible
+          &&
+          <>
+            <Clock location="New York" offset={-7} />
+            <Clock location="Kyiv" offset={0} />
+            <Clock location="London" offset={-2} />
+          </>
+        }
+      </div>
+    </>
   );
 };
 export default App;
--- lesson14/task5/src/Clock.jsx
+++ lesson14/task5/src/Clock.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import moment from 'moment';
+import { useState, useEffect } from 'react';
 
-const Clock = ({location, offset}) => {
-  let localeTime = new Date().setHours(new Date().getHours() + offset);
-  localeTime = new Date(localeTime); 
+const Clock = ({ location, offset }) => {
+  const [currentTime, setCurrentTime] = useState(new Date());
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+    return () => { clearInterval(interval) };
+  }, [currentTime]);
+  let localeTime = currentTime.setHours(currentTime.getHours() + offset);
+  localeTime = new Date(localeTime);
   return (
-    <>
+    <div className="clock__block">
       <div className="clock__location">{location}</div>
-  <div className="clock__time">{localeTime}</div>
-    </>
+      <div className="clock__time">{moment(localeTime).format('LTS')}</div>
+    </div>
   );
 };
 export default Clock;
