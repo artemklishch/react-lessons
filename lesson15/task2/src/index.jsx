9fc8ced66b3bbbb6214351a8cb26dcee8403c9b5
Artem <artemklishch@gmail.com>
2020-03-16 22:47:43 +0200

    e

---
+++ lesson15/task2/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Message from './Message';
+
+ReactDOM.render(<Message />, document.querySelector('#root'));
