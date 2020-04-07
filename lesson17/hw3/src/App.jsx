6312745d7b067e6947908cde6dc84b22af94df76
Artem <artemklishch@gmail.com>
2020-04-07 10:20:07 +0300

    rrsdfg

--- lesson17/hw3/src/App.jsx
+++ lesson17/hw3/src/App.jsx
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import User from './User';
 
-const App = () => {
-  return (
-    <div>Hello</div>
-  );
+class App extends Component {
+  state = {
+    userData: null,
+  };
+  componentDidMount() {
+    this.fetchGetData('facebook');
+  }
+  fetchGetData(userId) {
+    fetch(`https://api.github.com/users/${userId}`)
+      .then(response => {
+        if (response.ok) return response.json();
+        throw new Error('some error');
+      })
+      .then(data => this.setState({
+        userData: data,
+      }));
+  }
+  render() {
+    const { userData } = this.state;
+    if (!userData) return null;
+    return (
+      <div className="page">
+        <div className="page__content">
+          <h1>Users</h1>
+          <ul className="navigation">
+            <li className="navigation__item">
+              <button onClick={() => this.fetchGetData('github')} href="/users/github">Github</button>
+            </li>
+            <li className="navigation__item">
+              <button onClick={() => this.fetchGetData('facebook')} href="/users/facebook">Facebook</button>
+            </li>
+          </ul>
+        </div>
+        <User {...userData} />
+      </div>
+    );
+  }
 };
 export default App;
