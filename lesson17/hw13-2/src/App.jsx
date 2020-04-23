ff37599be491a8b4df45d7a78027a09716c8d849
Artem <artemklishch@gmail.com>
2020-04-23 11:51:51 +0300

    sdf

---
+++ lesson17/hw13-2/src/App.jsx
@@ -0,0 +1,11 @@
+import React, { useReducer } from 'react';
+import DataFetchingOne from './DataFetchingOne';
+
+const App = () => {
+  return (
+    <div>
+      <DataFetchingOne />
+    </div>
+  );
+};
+export default App;
