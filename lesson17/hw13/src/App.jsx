66787bf9089faf98ec210b3e033893952ae5553a
Artem <artemklishch@gmail.com>
2020-04-23 11:11:57 +0300

    rt

---
+++ lesson17/hw13/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useReducer } from 'react';
+import ComponentA from './ComponentA'
+import ComponentB from './ComponentB'
+import ComponentC from './ComponentC'
+
+const App = () => {
+  return (
+    <div>
+      Hello world
+    </div>
+  );
+};
+export default App;
