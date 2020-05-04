91558d2fa4910f1b91eb4da670db005258eaae8f
Artem <artemklishch@gmail.com>
2020-05-04 14:54:56 +0300

    fxdg

--- lesson18/hw1/src/store/actions/projectActions.js
+++ lesson18/hw1/src/store/actions/projectActions.js
@@ -5,9 +5,22 @@
 //   }
 // };
 
+import { firestore } from "firebase";
+
 export const createProject = project => {
-  return (dispatch, getState) => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
     //async call to database
-    dispatch({type: 'CREATE_PROJECT', project})
+    const firestore = getFirestore();
+    firestore.collection('projects').add({
+      ...project,
+      authFirstName: 'Net',
+      authLastName: 'Ninja',
+      authId: 12345,
+      createdAt: new Date(),
+    }).then(() => {
+      dispatch({ type: 'CREATE_PROJECT', project })
+    }).catch(err => {
+      dispatch({type: 'CREARE_PROJECT_ERROR', err})
+    });
   }
 };
