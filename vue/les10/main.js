cb5ed98200de74220862565ea6400d91a9b9c7ab
Artem <artemklishch@gmail.com>
2020-05-05 11:50:20 +0300

    вап

--- vue/les10/main.js
+++ vue/les10/main.js
@@ -4,5 +4,9 @@ const app = new Vue({
     product: 'Socks',
     image: './assets/Socks-Free-Download-PNG.png',
     url: 'https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding/',
+    inStock: false,
+    inNewStock: false,
+    inventory: 20,
+    onSale: true,
   },
 });
