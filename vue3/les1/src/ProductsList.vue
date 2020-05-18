c2ab845daa6d0df1379308d1b46e895cd9766a15
Artem Klishch <artemklishch@gmail.com>
2020-05-18 12:18:58 +0300

    dsf

--- vue3/les1/src/ProductsList.vue
+++ vue3/les1/src/ProductsList.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <router-link to="/add?text=hi" class="btn btn-success">Add Product</router-link>
+    <router-link to="/add?text=hi" class="btn btn-success"
+      >Add Product</router-link
+    >
     <ProductItem
       v-for="(product, i) in products"
       :key="product.id"
@@ -12,17 +14,27 @@
 <script>
 import ProductItem from "./ProductItem.vue";
 import ProductService from "./ProductService";
+import { mapGetters } from "vuex";
 export default {
   data() {
-    return {
-      products: []
-    };
+    return {};
   },
+  computed: mapGetters({
+    products: "getProducts"
+  }),
+  // computed: {
+  //   products() {
+  //     // return this.$store.state.products;
+  //     return this.$store.getters.getProducts;
+  //   }
+  // },
   components: {
     ProductItem
   },
   created() {
-    this.products = ProductService.products;
+    // this.products = ProductService.products;
+    // this.$store.commit("loadProducts", ProductService.products);
+    this.$store.dispatch("loadProducts", ProductService.products);
   }
 };
 </script>
--- vue3/les1/src/main.js
+++ vue3/les1/src/main.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import { router } from "./router";
+import store from './store/store'
 
 new Vue({
   router,
+  store,
   el: "#app1",
   data: {},
   render: h => h(App)
