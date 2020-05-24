d0da0d85222ae2bcc4759a80a14603b16ea79b29
Artem Klishch <artemklishch@gmail.com>
2020-05-24 13:50:34 +0300

    dfg

---
+++ vue3/les9/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <Component1></Component1>
+  </div>
+</template>
+
+<script>
+import Component1 from './components/Component1'
+
+export default {
+  components:{
+    Component1
+  }
+}
+</script>
+
+<style>
---
+++ vue3/les9/src/components/Component1.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <label for="name">Name</label>
+    <input type="text" id="name" v-model="name" />
+    <button @click="save">Save</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+    };
+  },
+  methods: {
+    save() {},
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  width: 600px;
+  height: 50px;
+  background-color: rgb(144, 207, 207);
+  margin-bottom: 10px;
+  vertical-align: middle;
+  padding-top: 10px;
+}
+</style>
